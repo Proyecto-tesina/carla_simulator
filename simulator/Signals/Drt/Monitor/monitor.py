94c1dd95a8af7f1aa81d30f7a43a1aa1bbe7b4a4
Leonel Mandarino <leonelmandarino@gmail.com>
2020-02-15 11:05:27 -0300

    Add monitor for DRT experiment

---
+++ simulator/Signals/Drt/Monitor/monitor.py
@@ -0,0 +1,25 @@
+from .alert_timeline import AlertTimeLine
+from .parser import MonitorParser
+
+
+class Monitor:
+    def __init__(self):
+        self.current_timeline = None
+        self.alert_timelines = []
+        self.parser = MonitorParser(path='simulator/monitor_results.json')
+
+    def add_turn_on_timestamp(self):
+        self.current_timeline = AlertTimeLine()
+
+    def add_turn_off_timestamp(self):
+        self.current_timeline.set_turn_off_time()
+        self.alert_timelines.append(self.current_timeline)
+        self.parser.write_to_file(self.alert_timelines)
+
+    def add_mistake_timestamp(self):
+        self.current_timeline.add_mistake()
+
+    def add_light_lost_timestamp(self):
+        self.current_timeline.set_light_lost_time()
+        self.alert_timelines.append(self.current_timeline)
+        self.write_to_file(self.alert_timelines)
--- simulator/Signals/Drt/modes.py
+++ simulator/Signals/Drt/modes.py
@@ -1,27 +1,40 @@
-import random
-
-import time
 import logging
-
+import random
+import time
+from abc import ABC, abstractmethod
 from threading import Thread
 
-from abc import ABC, abstractmethod
+from .Monitor.monitor import Monitor
 
 
 class Mode(ABC):
     def __init__(self, drt):
         self.drt = drt
+        self.monitor = Monitor()
+        self.mode_name = None
 
     @classmethod
     def build(self, drt):
-        if drt.config.mode_name() == "random":
+        self.mode_name = drt.config.mode_name()
+
+        if self.mode_name == "random":
             return RandomMode(drt)
         else:
             return ManualMode(drt)
 
-    @abstractmethod
     def toggle(self):
-        pass
+        if self.drt.is_rendered:
+            self.turn_off_light()
+        else:
+            self.turn_on_light()
+
+    @abstractmethod
+    def turn_off_light(self):
+        self.monitor.add_turn_off_timestamp()
+
+    @abstractmethod
+    def turn_on_light(self):
+        self.monitor.add_turn_on_timestamp()
 
 
 class RandomMode(Mode):
@@ -33,29 +46,34 @@ class RandomMode(Mode):
 
         self.start_timer()
 
-    def toggle(self):
-        if self.drt.is_rendered:
-            self.turn_off()
-
-    def turn_off(self):
+    def turn_off_light(self):
+        super().turn_off_light()
         self.drt.turn_off()
         self.start_timer()
+
+    def turn_on_light(self):
+        # If you try to turn on light manually when in random mode it will count as mistake
+        self.monitor.add_mistake_timestamp()
+        logging.info('The light wasn\'t on, be careful!')
 
     def start_timer(self):
         Thread(target=self._schedule_light).start()
 
     def _schedule_light(self):
         wait_time = random.randint(*self.interval)
-        logging.info(f'New shedulling on: {wait_time}')
+        logging.info(f'Making new shedulling for {wait_time}s')
 
         time.sleep(wait_time)
         self.drt.turn_on()
+        self.monitor.add_turn_on_timestamp()
 
 
 class ManualMode(Mode):
 
-    def toggle(self):
-        if self.drt.is_rendered:
-            self.drt.turn_off()
-        else:
-            self.drt.turn_on()
+    def turn_off_light(self):
+        super().turn_off_light()
+        self.drt.turn_off()
+
+    def turn_on_light(self):
+        super().turn_on_light()
+        self.drt.turn_on()
