77d0c51a5f2ec3f05f05783e96147412860da24e
Leonel Mandarino <leonelmandarino@gmail.com>
2020-02-27 12:15:46 -0300

    Add simplified data log for DRT

--- simulator/Signals/Drt/monitor/monitor.py
+++ simulator/Signals/Drt/monitor/monitor.py
@@ -1,25 +1,31 @@
-from .alert_timeline import AlertTimeLine
-from .serializer import MonitorSerializer
+import json
+from datetime import datetime
 
 
 class Monitor:
     def __init__(self):
-        self.current_timeline = None
-        self.alert_timelines = []
-        self.parser = MonitorSerializer(path='simulator/monitor_results.json')
+        self.events = []
+        self.path = 'simulator/monitor_results.json'
 
     def add_turn_on_timestamp(self):
-        self.current_timeline = AlertTimeLine()
-
-    def add_turn_off_timestamp(self):
-        self.current_timeline.set_turn_off_time()
-        self.alert_timelines.append(self.current_timeline)
-        self.parser.write_to_file(self.alert_timelines)
+        event = (datetime.now().isoformat(), 'start')
+        self.events.append(event)
 
     def add_mistake_timestamp(self):
-        self.current_timeline.add_mistake()
+        event = (datetime.now().isoformat(), 'mistake')
+        self.events.append(event)
 
     def add_light_lost_timestamp(self):
-        self.current_timeline.set_light_lost_time()
-        self.alert_timelines.append(self.current_timeline)
-        self.write_to_file(self.alert_timelines)
+        event = (datetime.now().isoformat(), 'lost')
+        self.events.append(event)
+        self.write_to_file(self.events)
+
+    def add_turn_off_timestamp(self):
+        event = (datetime.now().isoformat(), 'end')
+        self.events.append(event)
+        self.write_to_file(self.events)
+
+    def write_to_file(self, events):
+        with open(self.path, 'w') as file:
+            json_str = json.dumps(events, indent=4)
+            file.write(json_str)
