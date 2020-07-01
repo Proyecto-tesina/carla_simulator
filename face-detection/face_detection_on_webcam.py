cc239212224d48d752350c71a61f8645eb909e75
Leonel Mandarino <leonelmandarino@gmail.com>
2020-07-01 19:09:59 -0300

    Add log of face detector

--- face-detection/face_detection_on_webcam.py
+++ face-detection/face_detection_on_webcam.py
@@ -1,12 +1,24 @@
 import cv2
+import json
+from datetime import datetime
 
-face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+    raise RuntimeError("Cannot open camera")
+
+events = []
+
+
+def write_to_file():
+    with open('detection_results.json', 'w') as file:
+        json_str = json.dumps(events, indent=4)
+        file.write(json_str)
+
+
+wasFaceLastIteration = False
 
 while True:
     # Capture frames
@@ -14,13 +26,23 @@ while True:
 
     # if frame is read correctly ret is True
     if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+        print("Can't receive frame. Exiting ...")
         break
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detecta las caras y dibujo los cuadrados
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    if len(faces) != 0:
+        if not wasFaceLastIteration:
+            events.append((datetime.now().isoformat(), 'I see you'))
+            write_to_file()
+            wasFaceLastIteration = True
+    else:
+        if wasFaceLastIteration:
+            events.append((datetime.now().isoformat(), 'I don\'t see you'))
+            write_to_file()
+            wasFaceLastIteration = False
+
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
