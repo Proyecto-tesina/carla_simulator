70bb673bb5c632be05d61249dc9dae84256a7dba
Andrés Milla <andressmilla@gmail.com>
2020-09-02 15:32:28 -0300

    Fix wrong XML paths of face detector

--- face-detector/face_detection_on_webcam.py
+++ face-detector/face_detection_on_webcam.py
@@ -5,9 +5,8 @@ from datetime import datetime
 BASE_URL = 'http://127.0.0.1:8000'
 EXPERIMENT_ID = rq.get(f'{BASE_URL}/experiments/last').json()['id']
 
-face_cascade = cv2.CascadeClassifier(
-    'face-detector/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('face-detector/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('./haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
@@ -59,13 +58,14 @@ while True:
             wasFaceLastIteration = False
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
 
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            cv2.rectangle(roi_color, (ex, ey),
+                          (ex + ew, ey + eh), (0, 255, 0), 2)
 
     cv2.imshow('img', frame)
     if cv2.waitKey(1) == ord('q'):
